b602b8438fdf677181dd89cc057cf55606ccd63e
Tamayo <artamayo@cps.k12.il.us>
2024-05-06 12:13:16 -0500

    whileLoops

---
+++ whileLoops.py
@@ -0,0 +1,30 @@
+# nums = [1, 2, 3, 4, 5]
+# # nested for loop
+# for num in nums:
+#     for letter in 'abc':
+#         print(num, letter)
+
+
+
+# # ranges
+# for i in range( 421):
+#     print(i)
+
+# while loops
+# x = 0 
+
+# while True:
+#     if x == 5:
+#         break
+#     print(x)
+#     x += 1
+
+# while True:
+#     print("Enter your name:")
+#     name = input()
+#     if name == 'Andrea':
+#         print("your name is" +name)
+#         break
+#     else:
+#         print("Thank you!")
+
